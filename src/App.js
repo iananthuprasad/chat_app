c318fdff2670dff0385f751e337e0786c88afe4b
iananthuprasad.com <iananthuprasad@gmail.com>
2023-12-24 19:03:13 +0530

    yeye

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ import { auth } from "./firebase";
 const App = () => {
   const [user] = useAuthState(auth);
   return (
-    <div className="app">
-      <header></header>
-      <section>
-        {user? <Chatroom/>:<Sign/>}
-      </section>
+    <div className="App">
+      <header>
+        <h1>⚛️🔥💬</h1>
+      </header>
+
+      <section>{user ? <Chatroom /> : <Sign />}</section>
     </div>
   );
 };
